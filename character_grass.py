4c598792d4979776408cbfe41d6fd96c21d038a1
Rungbam(main) <marungbam@naver.com>
2023-09-21 11:52:21 +0900

    원 그리고 사각형 그리는 것이 무한반복

--- character_grass.py
+++ character_grass.py
@@ -10,6 +10,16 @@ grass.draw_now(400, 30)
 character.draw_now(400, 90)
 delay(1)
 
+def run_circle() :
+    print('Circle')
+    pass
 
+def run_rectangle() :
+    print('rectangle')
+    pass
+
+while True :
+    run_circle()
+    run_rectangle()
 
 close_canvas()
